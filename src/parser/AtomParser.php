f488c7783f8d825688d4182dbdbbe864507edabd
Konstantin Baierer <konstantin.baierer@bib.uni-mannheim.de>
2015-09-24 11:47:23 +0200

    AtomParser to parse atom feeds

---
+++ src/parser/AtomParser.php
@@ -0,0 +1,23 @@
+<?php
+
+class AtomParser
+{
+
+    // override
+    public function parse(Session $session)
+    {
+        $chan = new DOMDocument();
+        $chan->loadXML(mb_convert_encoding($session->bytes, 'HTML-ENTITIES', 'UTF-8'));
+        $sheet = new DOMDocument();
+        $sheet->load('data/atom2rss/atom2rss.xsl'); /* use stylesheet from this page */
+        $processor = new XSLTProcessor();
+        $processor->registerPHPFunctions();
+        $processor->importStylesheet($sheet);
+        $session->dom = $processor->transformToDoc($chan);
+        $session->xpath = new DOMXPath($session->dom);
+        $session->dom->save('/tmp/atom.xml');
+        // error_log("RESULT: " . $session->dom->saveXML());
+    }
+}
+
+?>
